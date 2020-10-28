931477b39b9026ff084f2a0897386d2ab24c669e
Vedant <69904231+VedantWHjr@users.noreply.github.com>
2020-10-28 18:00:48 +0530

    Update Paper.js

--- Paper.js
+++ Paper.js
@@ -1,13 +1,19 @@
 class Paper{
 constructor(x,y){
+    
+    
     var options={
         isStatic : false,
         restitution:0.3,
         friction:0.5,
         density:1.2
     }
+    this.body = Bodies.circle(x,y,options);
     
 }
+display(){
+this.body.display();
 
+}
 
 }
